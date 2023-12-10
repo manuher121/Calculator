56f19e0cf90444be5500c76d6d672f243b97a304
manuel <manuher1229@hotmail.com>
2023-12-10 03:59:14 -0500

    A lot of changes were made
    HTML File updated to display everything correclty.
    CSS Stylesheet added to make it look better.
    JS File functions added, a number is returned when pressing a button.

--- java.js
+++ java.js
@@ -31,3 +31,49 @@ function operate(firstNumber, secondNumber, operator) {
         console.log("Operator not recognized");
     }
 }
+
+let numberToDisplay = 0;
+
+let content = document.getElementById("resultDisplay");
+
+const buttons = document.querySelectorAll(".number");
+
+
+function displayNumber() {
+    if (numberToDisplay > 999999999999) {
+        content.textContent="ERROR";
+
+    }
+    else {
+        content.textContent=numberToDisplay;
+    }
+}
+
+///numbersButton.onclick = function() {
+///    console.log(numbersButton.textContent);
+///}
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      // do something when the button is clicked
+      console.log(button.textContent);
+    });
+  });
+
+
+
+
+
+
+function changeNumbers(onClickNumber) {
+    if (numberToDisplay == 0) {
+        numberToDisplay = onClickNumber;
+    }
+    else {
+        return numberToDisplay + onClickNumber
+    }
+}
+
+
+
+
+displayNumber();
